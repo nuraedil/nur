18da27d730ecf271493293ed8b82cecc9c3139e0
nuraedil <nur_mustafa@kbtu.kz>
2024-01-19 00:22:49 +0600

    Initial Commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+print("Algashky sandy jazynyz")
+a = int(input())
+print("Ekinshi sandy jazynyz")
+b = int(input())
+
+if a > b:
+    print("Algashky san ulken")
+if a==b:
+    print("Ten bolady")
+else:
+    print("Ekinshi san ulken")
