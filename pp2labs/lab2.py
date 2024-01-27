46eda656353bbc59b9357c8ac0d01a61eff5cac1
nuraedil <nur_mustafa@kbtu.kz>
2024-01-27 16:27:44 +0600

    lab

--- pp2labs/lab2.py
+++ pp2labs/lab2.py
@@ -68,7 +68,6 @@ print(bool("abc"))
 print(bool(0))
 
 
-
 #Python operators
 
 #1
@@ -93,6 +92,32 @@ if(5 != 10):
 #ex5
 if 5 == 10 or 4 == 4:
     print("At least one statement is true")
+
+
+#Python lists
+
+#1
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+#2
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+#3
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+#4
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+#5
+list1 = ["abc", 34, True, 40, "male"]
+#6
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+#7
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
 
     
 
