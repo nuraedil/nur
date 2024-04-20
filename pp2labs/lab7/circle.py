fb725397cae71d8ba72baa9a14325374ae65bc07
nuraedil <nur_mustafa@kbtu.kz>
2024-04-20 17:06:37 +0500

    circle

--- pp2labs/lab7/circle.py
+++ pp2labs/lab7/circle.py
@@ -2,33 +2,27 @@ import pygame
 
 pygame.init()
 
-# Определяем размеры экрана и создаем его
 WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Устанавливаем заголовок окна
 pygame.display.set_caption("Moving Circle")
 
-# Цвета
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-# Начальные координаты и скорость перемещения круга
 x, y = WIDTH // 2, HEIGHT // 2
 speed = 20
 
-# Основной цикл программы
+
 running = True
 clock = pygame.time.Clock()
 while running:
-    screen.fill(WHITE)  # Очищаем экран белым цветом
+    screen.fill(WHITE)  
 
-    # Обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Проверяем нажатые клавиши
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP] and y >= 41:
         y -= speed
@@ -39,10 +33,9 @@ while running:
     if keys[pygame.K_RIGHT] and x <= WIDTH - 41:
         x += speed
 
-    # Рисуем круг
     pygame.draw.circle(screen, RED, (x, y), 25)
 
-    pygame.display.update()  # Обновляем экран
-    clock.tick(60)  # Устанавливаем FPS
+    pygame.display.update()  
+    clock.tick(60) 
 
 pygame.quit()
